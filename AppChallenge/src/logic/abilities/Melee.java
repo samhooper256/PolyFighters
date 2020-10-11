f40589da3c13222752c582d3f3dd35237114d42c
samhooper256 <samhooper256@gmail.com>
2020-10-11 07:24:14 -0500

    added inBounds method to Board and converted Melee method over

--- AppChallenge/src/logic/abilities/Melee.java
+++ AppChallenge/src/logic/abilities/Melee.java
@@ -50,19 +50,19 @@ public class Melee extends AbstractAnyAbility implements SingleProjectileAbility
 		{
 			for(int i = uRow - 1; i < uRow + 2; i++)
 			{
-				if(isInBounds(i, uCol - 1))
+				if(b.inBounds(i, uCol - 1))
 				{
 					int[] arrC1 = new int[] {i, uCol - 1};
 					arr.add(arrC1);
 				}
 				
-				if(isInBounds(i, uCol))
+				if(b.inBounds(i, uCol))
 				{
 					int[] arrC2 = new int[] {i, uCol};
 					arr.add(arrC2);
 				}
 				
-				if(isInBounds(i, uCol + 1))
+				if(b.inBounds(i, uCol + 1))
 				{
 					int[] arrC3 = new int[] {i, uCol + 1};
 					arr.add(arrC3);
@@ -73,18 +73,6 @@ public class Melee extends AbstractAnyAbility implements SingleProjectileAbility
 		return arr;
 	}
 	
-	private boolean isInBounds(int row, int col)
-	{
-		Board b = unit.getBoard();
-		int rows = b.getRows();
-		int cols = b.getCols();
-		if((row >= 0 && row < rows) && (col >= 0 && col < cols))
-		{
-			return true;
-		}
-		
-		return false;
-	}
 	@Override
 	public boolean canTarget(GameObject object) {
 		return object instanceof Unit || object instanceof Obstacle;
